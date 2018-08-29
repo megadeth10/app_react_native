134046e0e6fe3fb2f4f07bd8f4e3c54b0df10943
megadeth10 <1metallica@hanmail.net>
2018-08-29 14:09:54 +0900

    1. android 코드 분리함

--- src/component/fcm/byMessaging.js
+++ src/component/fcm/byMessaging.js
@@ -1,15 +1,18 @@
 import firebase from 'react-native-firebase';
 import { RemoteMessage } from 'react-native-firebase';
-
+import { Platform } from 'react-native';
 export default async (message) => {
     console.log(message);
-
     const notification = new firebase.notifications.Notification();
-    notification.setNotificationId("1");
-    notification.setTitle("백그라운드 메시지");
-    notification.setBody("되나요??");
-    notification.android.setChannelId("리액트 테스트");
-    
+    if (Platform.OS === "android"){
+        notification.setNotificationId("1");
+        notification.setTitle("백그라운드 메시지");
+        notification.setBody("되나요??");
+
+        if (Platform.Version >= 26) {//set channel
+            notification.android.setChannelId("리액트 테스트");
+        }
+    }
     firebase.notifications().displayNotification(notification);
 
     return Promise.resolve();
