88a878834bf0bdc24df47cb24ff389ad5e39eb3c
megadeth10 <1metallica@hanmail.net>
2018-09-12 10:33:41 +0900

    1. scrollView 에서 자식 node offset 값 계산 하여 스크롤 이동 수정

--- src/component/navigation/InputScreen.js
+++ src/component/navigation/InputScreen.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, ScrollView, ViewPagerAndroid, StyleSheet, Dimensions, Image, TouchableOpacity, TextInput, Keyboard, AsyncStorage } from 'react-native';
+import { View, Text, ScrollView, ViewPagerAndroid, StyleSheet, Dimensions, Image, TextInput, Keyboard, AsyncStorage } from 'react-native';
 import { Container, Header, Body, Title, Content, Left, Right, Icon, Button } from 'native-base';
 import NavigationService from './NavigationService';
 import update from 'immutability-helper';
 import _ from 'lodash';
 import DeviceUtil from '../utils/DeviceUtil';
+import ImageWithConstraints from "../view/ImageWithConstraints";
 
 const propTypes = {
 }
 
 const defaultProps = {
 }
+
+const image = require("../../img/login_img_default.png");
 
 const STORE_KEY = "INPUT_DATA";
 class InputScreen extends Component {
@@ -55,7 +58,32 @@ class InputScreen extends Component {
                         <Title style={ { color: "#000000" } }>입력 화면</Title>
                     </Body>
                 </Header>
-                <ScrollView style={ { flex: 1 } }>
+                <ScrollView style={ { flex: 1 } } ref="scrollView">
+                    <View ref="mainImage"
+                        onLayout={ this.handleLayoutChange }>
+                        <ImageWithConstraints
+                            source={ image }
+                            style={
+                                {
+                                    width: "100%"
+                                }
+                            }
+                            originalWidth={ 360 }
+                            originalHeight={ 326 }
+                            resizeMode="contain" />
+                    </View>
+                    <View ref="subImage"
+                        onLayout={ this.handleLayoutChange2 }>
+                        <Image
+                            source={ { uri: "http://image.ddingdong.net:8083/vendor/F0000000000000004153.jpg" } }
+                            style={
+                                {
+                                    width: 87,
+                                    height: 56
+                                }
+                            }
+                            resizeMode="contain" />
+                    </View>
                     <Text>{ inputText }</Text>
                 </ScrollView>
                 <TextInput style={ { width: "100%", borderWidth: 1 } } editable={ true }
@@ -64,11 +92,52 @@ class InputScreen extends Component {
                     ref="inputText"
                 />
                 <Button style={ { width: "100%", justifyContent: "center" } }
+                    onPress={ this.goMainImage }>
+                    <Text>goto main image</Text>
+                </Button>
+                <Button style={ { width: "100%", justifyContent: "center" } }
+                    onPress={ this.goSubImage }>
+                    <Text>goto sub image</Text>
+                </Button>
+                <Button style={ { width: "100%", justifyContent: "center" } }
                     onPress={ this.onPress }>
                     <Text>add text</Text>
                 </Button>
             </Container>
         );
+    }
+
+    //TODO ScrollList에서 child Component의 offset을 어떻게 구하는가??????????????????(9.11)
+    goMainImage = (e) => {
+        if (this.refs.mainImage) {
+            const { mainImage } = this.offset;
+            this.refs.scrollView.scrollTo({ x: 0, y: mainImage, animated: true });
+        }
+    }
+
+    handleLayoutChange = () => {
+        this.refs.mainImage.measure((fx, fy, width, height, px, py) => {
+            console.log('Main Component height is: ' + height);
+            console.log('Main Y offset to page: ' + py);
+            console.log('Main Y offset to frame: ' + fy);
+            this.offset = { ...this.offset, ["mainImage"]: py };
+        })
+    }
+
+    handleLayoutChange2 = () => {
+        this.refs.subImage.measure((fx, fy, width, height, px, py) => {
+            console.log('Sub Component height is: ' + height);
+            console.log('Sub Y offset to page: ' + py);
+            console.log('Sub Y offset to frame: ' + fy);
+            this.offset = { ...this.offset, ["subImage"]: py };
+        })
+    }
+
+    goSubImage = (e) => {
+        if (this.refs.subImage) {
+            const { subImage, mainImage } = this.offset;
+            this.refs.scrollView.scrollTo({ x: 0, y: subImage - mainImage, animated: true });
+        }
     }
 
     _retrieveData = async () => {
