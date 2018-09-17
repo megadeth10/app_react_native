f210132ba3d07d1c060eb845de641baacb92ba6d
megadeth10 <1metallica@hanmail.net>
2018-09-17 15:44:00 +0900

    1.  image resizer 관련 content 수정함.

--- src/component/navigation/CameraScreen.js
+++ src/component/navigation/CameraScreen.js
@@ -4,7 +4,7 @@ import { Button, View, Text, Image, Platform, Alert } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 import ImageResizer from 'react-native-image-resizer';
 /**
- * ImageResizer.java
+ * ImageResizer.java (for Android)
  * createResizedImage() method에 아래 코드 수정하여 file name 유지 하도록 변경함.
  * //        if (outputPath != null) {
  * //            path = new File(outputPath);
